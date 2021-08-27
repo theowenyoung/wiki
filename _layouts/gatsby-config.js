9db45e738d3a39776ddacd967988d277c8148fb9
Owen Young <theowenyoung@gmail.com>
2021-08-27 10:25:34 +0800

    Init wiki

---
+++ _layouts/gatsby-config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const pathPrefix = "/";
+const siteMetadata = {
+  title: "Everything I Know by Owen",
+  shortName: "Wiki",
+  description:
+    "Personal wiki, share everything I learned, and spend a lifetime updating it. I hope it'll be a good gift for my grandchildren.",
+  siteUrl: "https://wiki.owenyoung.com",
+  twitterName: "theowenyoung",
+};
+module.exports = {
+  siteMetadata,
+  pathPrefix,
+  flags: {
+    DEV_SSR: true,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `${__dirname}/..`,
+        ignore: [`**/\.*`, "**/_layouts/**"],
+      },
+    },
+    {
+      resolve: "gatsby-theme-primer-wiki",
+      options: {
+        nav: [
+          {
+            title: "Github",
+            url: "https://github.com/theowenyoung/wiki",
+          },
+        ],
+        sidebarDepth: 2,
+        editUrl: "https://github.com/theowenyoung/wiki/tree/main/",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: siteMetadata.title,
+        short_name: siteMetadata.shortName,
+        start_url: pathPrefix,
+        background_color: `#f7f0eb`,
+        display: `standalone`,
+        icon: path.resolve(__dirname, "./static/logo.png"),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: siteMetadata.siteUrl,
+        sitemap: `${siteMetadata.siteUrl}/sitemap/sitemap-index.xml`,
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+  ],
+};
